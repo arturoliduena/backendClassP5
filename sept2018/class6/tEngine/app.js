b98376229799f7f4a828fdbc8f2743c7504eaeca
Arturo <arturo.liduena@gmail.com>
2018-10-26 19:38:21 -0300

    init backend

--- sept2018/class6/tEngine/app.js
+++ sept2018/class6/tEngine/app.js
@@ -1,114 +1,70 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost/proyecto")
 
-app.set('view engine', 'ejs');
-let products = [
-    {
-        id: 1,
-        nombre: 'Iphone X',
-        precio: 1000,
-        descripcion: 'el nuevo iphone',
-        color: 'black',
-        url: 'https://clipart.info/images/ccovers/1505918647iphone-x-png.png'
-    },
-    {   
-        id: 2,
-        nombre: 'nokia 1100',
-        precio: 50,
-        descripcion: 'indestructible',
-        color: 'blue',
-        url:'http://wpc.72c72.betacdn.net/8072C72/lvi-images/sites/default/files/styles/landscape_1020_560/public/nota_periodistica/Nokia1100.jpg'
-    },
-    {
-        id: 3,
-        nombre: 'blu',
-        precio: 1,
-        descripcion: 'anda bien pero se calienta',
-        color: 'blue',
-        url:'https://cdn.shopify.com/s/files/1/1612/7137/products/BLU-Grand-5.5-HD-Gray-Image-HD-3-newst8_800x.jpg?v=1499968011'
-    }
-]
-app.get('/', function(req, res){
-    let obj = {
-        nombre: 'Arturo',
-        isSubscribe: true,
-        materias: ['matematica', 'fisica', 'castellano', 'quimica'],
-        notas: [10, 15, 20, 10, 5, 1]
-    };
-
-    let suma = obj.notas.reduce(function(acc, valor){
-        return acc + valor
-    })
-    let promedio = suma/obj.notas.length
-    obj.promedio = promedio;
-    console.log(obj)
-    res.render('index', obj)
+let productModel = new mongoose.Schema({
+    nombre: {type: String, required: true},
+    precio: Number,
+    descripcion: String,
+    color: String,
+    url: String,
 });
 
-app.get('/products', function(req, res){
-    res.render('products.ejs', { listProduct: products})
-})
+let Products = mongoose.model('products', productModel)
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    Products.find({}, (err, result) => {
+        res.render('products.ejs', { listProduct: result })
+    })
+});
 
 app.get('/item/:id', function(req, res){
-    let id = req.params.id
-    let product = null;
-    products.forEach(item => {
-        if(id == item.id) product = item
+    Products.findById(req.params.id, (err, result) => {
+        res.render('item', {item: result})
     })
-    res.render('item', {item: product})
 })
 
 app.get('/create', function(req, res){
-    let product = {
-        id: 0,
+    const product = {
+        _id: 0,
         nombre: '',
         precio: 0,
         descripcion: '',
         color: '',
-        url:''
+        url: ''
     }
     res.render('form', {item: product})
 })
 
 app.get('/update/:id', function(req, res){
-    let id = req.params.id
-    let product = null;
-    products.forEach(item => {
-        if(id == item.id) product = item
+    Products.findById(req.params.id, (err, result) => {
+        res.render('form', {item: result})
     })
-    res.render('form', {item: product})
 })
 
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.post('/createOrUpdate/:id', urlencodedParser,function(req, res){
-    let id = Number(req.params.id);
-    if(!id){
-        products.push(req.body)
-        req.body.id = products.length;    
-    }else {
-        let product = null;
-        products.forEach(item => {
-            if(id == item.id) {
-                item.nombre = req.body.nombre;
-                item.color = req.body.color;
-                item.descripcion = req.body.descripcion;
-                item.precio = req.body.precio;
-                item.url = req.body.url;
-            }
-        }) 
+    let id = req.params.id;
+    if(id == 0){
+        Products.create(req.body, (err, result) => {
+            res.redirect('/')
+        })
+    } else {
+        Products.update({_id: id}, req.body, (err, result) => {
+            res.redirect('/')
+        })
     }
-    res.redirect('/products')
 })
 
 app.get('/delete/:id', function(req, res){
-    let id = Number(req.params.id);
-    let index = products.findIndex(function(element){
-        return id == element.id
+    Products.deleteOne({_id: req.params.id}, (err, result) => {
+        res.redirect('/')
     })
-    products.splice(index, 1)
-    res.redirect('/products')
 })
 
 
