59aeaef048ca456e694b6017738c4fdec19d56b7
Arturo <arturo.liduena@gmail.com>
2018-11-07 21:21:50 -0300

    class6

--- oct2018/class5/expressTest/app.js
+++ oct2018/class5/expressTest/app.js
@@ -3,7 +3,10 @@ const app = express();
 const fs = require('fs');
 const bodyParser = require('body-parser');
 
+let alumnos = []
+
 app.use('/assets/', express.static(__dirname + '/public'))
+app.set('view engine', 'ejs');
 
 app.get('/home', (req, res) => {
     let file = fs.readFileSync(__dirname + '/index.html')
@@ -22,11 +25,26 @@ app.get('/formulario', (req, res) => {
 })
 
 
-let urlencodedParser = bodyParser.urlencoded({ extended: false })
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.post('/formulario', urlencodedParser, (req, res) => {
+    alumnos.push(req.body)
+    res.redirect('/testengine')
+})
+
+const jsonParser = bodyParser.json()
+app.post('/formulariojson', jsonParser, (req, res) => {
     console.log(req.body)
-    res.send('usuario registrado')
+    alumnos.push(req.body)
+    res.send('ok')
+})
+
+app.get('/alumnos', (req, res) => {
+    res.json(alumnos)
+})
+
+app.get('/testengine', (req, res) => {
+    res.render('listalum', {list: alumnos})
 })
 
 app.listen(3000, () => console.log('listening in port 3000'));
