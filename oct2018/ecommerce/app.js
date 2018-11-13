897a1c98e2fe4cfcac2e1015826b96cc963ce58c
Arturo <arturo.liduena@gmail.com>
2018-11-12 21:13:08 -0300

    class8

--- oct2018/ecommerce/app.js
+++ oct2018/ecommerce/app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
 
+let count = 1;
+
 let products = [
     {
         id: 1,
@@ -15,24 +17,55 @@ let products = [
 ];
 
 app.get('/', (req, res) => {
-    res.render('home', { products })
+    res.render('home.ejs', { products })
 })
 
-app.get('/create', (req, res) => {
-    res.render('form')
+app.get('/add/:code', (req, res) => {
+    let product = null;
+    if(req.params.code != 0){
+        product = products.find(function(element) {
+            return element.id == req.params.code;
+          });
+    } else {
+        product = {
+            id: 0,
+            nombre: '',
+            precio: 0,
+            descripcion: '',
+            url: ''
+        }
+    }
+    
+    res.render('form.ejs', product)
 })
 
 const urlencoded = bodyParser.urlencoded({extend: false}) 
-app.post('/create', urlencoded, (req, res) => {
-    products.push(req.body)    
-    req.body.id = products.length
+app.post('/add/:code', urlencoded, (req, res) => {
+    if(req.params.code != 0) {
+        let found = products.find(function(element) {
+            return element.id == req.params.code;
+        });
+        
+        found = Object.assign(found, req.body)
+        
+    } else {
+        products.push(req.body);
+        req.body.id = ++count
+    }
+    
     res.redirect('/')
 })
 
-app.get('/product/:id', (req, res) => {
-    var found = products.find(function(element) {
-        return element.id == req.params.id;
+app.get('/product/:code', (req, res) => {
+    const found = products.find(function(element) {
+        return element.id == req.params.code;
       });
-      res.render('item', found)
+      res.render('item.ejs', found)
+})
+
+app.get('/delete/:prodId', (req, res) =>{
+    const index = products.findIndex(element => element.id == req.params.prodId)
+    products.splice(index, 1)
+    res.redirect('/')
 })
 app.listen(4000, () => console.log('listening in port 4000'))
